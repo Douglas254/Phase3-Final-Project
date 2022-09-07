7ad90209cdc302ad2f37f73bd518b85c1667d19d
Douglas Obara <douglasobara97@gmail.com>
2022-09-07 16:44:05 +0300

    add delete and edit action btn

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
+import { BsFillPencilFill } from "react-icons/bs";
+import { BsFillTrashFill } from "react-icons/bs";
 
-const Post = ({posts}) => {
-
+const Post = ({ posts }) => {
   return (
     <>
       <div className="row">
         {posts.map(({ title, image, short_story }) => (
           <div className="col-md-4 ">
             <div className="post__wrapper">
-              <h3 className='p-2 text-center'>{title}</h3>
+              <h3 className="p-2 text-center">{title}</h3>
               <div className="post__image">
                 <img src={image} alt={title}></img>
               </div>
               <p className="story__desc">{short_story}</p>
+              <div className="action__icons">
+                <div className="post__edit">
+                  <BsFillPencilFill />
+                </div>
+                <div className="post__delete">
+                  <BsFillTrashFill />
+                </div>
+              </div>
             </div>
           </div>
         ))}
       </div>
     </>
   );
-}
+};
 
-export default Post
+export default Post;
